8d070df456225e7e30ac943072d901a04c5669f7
yeren <yeren@appledeMacBook-Air.local>
2022-05-12 15:51:26 +0800

    堆排序C语言实现 *

---
+++ heapsort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void heap_sort(int *array, int n) {
+
+}
+
+void down_to_top_adjust(int *array, int index) {
+    int neighbour, parent;
+    if(index % 2 == 0)
+        neighbour = index - 1;
+    else
+        neighbour = index + 1;
+    parent = (index - 1)/2;
+    
+}
+
+int main(){
+
+}
